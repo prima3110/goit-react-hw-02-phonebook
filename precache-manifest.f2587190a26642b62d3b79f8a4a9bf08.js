c846e30828ddc4f0f502b05fe0305dc010e6b5c8
Roman Pryymak <prima3110@ukr.net>
2019-12-20 21:44:06 +0200

    Updates

---
+++ precache-manifest.f2587190a26642b62d3b79f8a4a9bf08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a59ebf4e6de0e600390558c674304a8",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "5ec54bfbeaaf89be8ece",
+    "url": "/goit-react-hw-02-phonebook/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "d2f2a81965e0c4935305",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.3b207201.chunk.css"
+  },
+  {
+    "revision": "5ec54bfbeaaf89be8ece",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.cad95b99.chunk.js"
+  },
+  {
+    "revision": "d2f2a81965e0c4935305",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.11d4d70b.chunk.js"
+  },
+  {
+    "revision": "0b962a7aa984f5309b42",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d477eed5.js"
+  }
+]);
